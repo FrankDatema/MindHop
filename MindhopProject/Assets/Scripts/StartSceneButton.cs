3a922c997c56435f9ab312cd86b22db6a22ffcd6
MONKEY\wbbla <wbblanksma@gmail.com>
2025-02-26 17:44:57 +0100

    [ADD] Quit function

--- MindhopProject/Assets/Scripts/StartSceneButton.cs
+++ MindhopProject/Assets/Scripts/StartSceneButton.cs
@@ -5,7 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneButton : MonoBehaviour
 {
+    //Switch scene when pressed Start
    public void PlayGame(){
     SceneManager.LoadSceneAsync("Game");
+   }
+   //Quit game when pressed Quit
+   public void QuitGame(){
+    Application.Quit();
    }
 }
